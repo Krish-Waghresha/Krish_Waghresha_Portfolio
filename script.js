bf5b57d9136195623aa0d39b2fc90f26b9f4249d
Krish-Waghresha <krishwaghresha@gmail.com>
2024-03-03 21:21:31 +0530

    The base code is been commited

---
+++ script.js
@@ -0,0 +1,7 @@
+var typed = new Typed(".multiple-text", {
+    strings:["       ", "Problem Solver","Tech Enthusiast","Developer","Passionate Learner"],
+    typeSpeed:90,
+    backSpeed:45,
+    backDelay:1000,
+    loop:true
+})
