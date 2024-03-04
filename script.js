ce4bef1537d9cda683bfa817160199d3e8677881
Krish-Waghresha <krishwaghresha@gmail.com>
2024-03-04 14:45:23 +0530

    Active Link JS

--- script.js
+++ script.js
@@ -1,7 +1,18 @@
-var typed = new Typed(".multiple-text", {
+let typed = new Typed(".multiple-text", {
     strings:["       ", "Problem Solver","Tech Enthusiast","Developer","Passionate Learner"],
     typeSpeed:90,
     backSpeed:45,
     backDelay:1000,
     loop:true
 })
+
+let navbar = document.getElementsByClassName("navbar");  
+for (let i = 0; i < navbar.length; i++) {  
+  navbar[i].addEventListener("click", function() {  
+
+    let current = document.getElementsByClassName("active");  
+    current[0].classList.remove("active");  
+    this.classList.add("active");  
+
+  });  
+}  
